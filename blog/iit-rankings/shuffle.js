6ae32c5349feebd710af6a8d9fe38b969aa91784
Neeldhara Misra <neeldhara.misra@gmail.com>
2022-11-25 11:44:50 +0530

    Ranking all IITs.

---
+++ blog/iit-rankings/shuffle.js
@@ -0,0 +1,22 @@
+export function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+// Used like so
+var arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);
